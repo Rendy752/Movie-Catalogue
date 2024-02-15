9cd2c54ae131ff597497f2abd49dde78ebd8a9a3
Rendy Pratama <rendypratama752@gmail.com>
2024-02-15 22:21:25 +0700

    ADD: upcoming, now playing, and detail movie

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '51395fed3641b2b2439a3222e1c159ff',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;

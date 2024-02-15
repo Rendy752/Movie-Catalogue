7557c96ab87f24f238638ef940dcdf2e157f08a8
Rendy Pratama <rendypratama752@gmail.com>
2024-02-15 23:13:20 +0700

    ADD: service worker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 
 import App from './views/app';
+import swRegister from './utils/sw-register';
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
@@ -16,6 +17,7 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', () => {
   app.renderPage();
+  swRegister();
 });
 
 export default app;
